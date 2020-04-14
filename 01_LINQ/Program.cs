252d65a2822e470fd6232705a0f8f3c155efc021
Joao Pedro <jp.ssilva@live.com>
2020-04-14 16:35:34 -0300

    Introdução ao LINQ

--- 01_LINQ/Program.cs
+++ 01_LINQ/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _01_LINQ
 {
@@ -10,6 +8,33 @@ namespace _01_LINQ
     {
         static void Main(string[] args)
         {
+            //LINQ E LAMBDA
+            //LAMBDA = (ENTRADA) => (EXPRESSÃO)
+            List<string> nomeLista = new List<string>() { "João", "Luana", "Alana", "Zoraide", "Kaká", "Cora", "Érica", "Matheus" };
+
+            int[] lista = { 3, 4, 5, 6, 10, 20, 30, 40, 50, 100, 25, 35, 74 };
+
+            var vetorListaOrdenada = lista.Where(a => a > 10).OrderBy(a => a).Select(a => a);
+
+            var nomeListaOrdenada = nomeLista.OrderBy(x => x).Select(x => x);
+
+            
+            foreach (var listaNomes in nomeListaOrdenada)
+            {
+                Console.WriteLine(listaNomes);
+
+            }
+
+            Console.WriteLine();
+
+            foreach (int vetorLista in vetorListaOrdenada)
+            {
+                Console.WriteLine(vetorLista);
+            }
+
+            
+            Console.ReadKey();
+
         }
     }
 }
