f1e7764815ea9fcd7ae910b6a5824b528eb2b123
Joao Pedro <jp.ssilva@live.com>
2020-04-14 17:17:35 -0300

    LINQ com Objeto

---
+++ 03_LINQ_OBJETO/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace _03_LINQ_OBJETO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            List<Usuario> listaUsuario = new List<Usuario>();
+
+            listaUsuario.Add(new Usuario("João", "joao@gmail.com"));
+            listaUsuario.Add(new Usuario("Erica", "erica@hotmail.com"));
+            listaUsuario.Add(new Usuario("Cássia", "cassia@gmail.com"));
+            listaUsuario.Add(new Usuario("Gilmar", "gilmar@bol.com"));
+            listaUsuario.Add(new Usuario("Lauany", "lauanay@gmail.com"));
+
+            var listaUsuarioFiltrada = listaUsuario.Where(x => x.Email.Contains("gmail.com")).OrderBy(x=> x.Nome).Select(x=>x);
+
+
+            foreach(var item in listaUsuarioFiltrada)
+            {
+                Console.WriteLine(item.Nome + " - " + item.Email);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 03_LINQ_OBJETO/Usuario.cs
@@ -0,0 +1,18 @@
+﻿namespace _03_LINQ_OBJETO
+{
+    class Usuario
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+        public Usuario(string nome, string email)
+        {
+            Nome = nome;
+            Email = email;
+        }
+
+       public Usuario()
+        {
+        }
+    }
+}
